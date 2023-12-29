ff6e993713e855992d950f0fe19be3d4064f6746
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-29 22:18:08 +0200

    Fix endpoints path

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
-from todo.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView, TagCreateView, \
-    TagUpdateView, TagDeleteView
+from todo.views import (
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+)
 
 app_name = "todo"
 
@@ -12,8 +20,6 @@ urlpatterns = [
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
-    path("tags/update/<int:pk>", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/delete/<int:pk>", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete", TagDeleteView.as_view(), name="tag-delete"),
 ]
-
-
